826d72b94eeb2ded49956792271afbe4c14f0b70
Tyler887 <tylermageeshields@gmail.com>
2022-03-31 23:06:51 +0100

    Update generate-home-buttons.py

--- generate-home-buttons.py
+++ generate-home-buttons.py
@@ -4,9 +4,9 @@ def addhome(page):
        with open(page, "a+") as file:
         if page.endswith(".md"):
          file.write(f"<a href=\"/typoqsuat\"><sub><< Home</sub></a>")
-         print(f"Added home button to {i}")
+         print(f"Added home button to {file}")
         else:
-         print(f"Skipped {i}")
+         print(f"Skipped {file}")
       else:
         for i in glob.glob(page):
           addhome(i)
