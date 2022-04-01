7aeffb7c569634bf8c6eb9658f28ef37f9b8598f
Tyler887 <tylermageeshields@gmail.com>
2022-04-01 11:58:41 +0100

    Update generate-home-buttons.py

--- generate-home-buttons.py
+++ generate-home-buttons.py
@@ -1,20 +1,21 @@
 import os, glob
+from colorama import *
 def addhome(page):
    try:
       if os.path.isfile(page):
        with open(page, "a+") as file:
         if page.endswith(".md"):
          file.write(f"\n\n[<sub><< Home</sub>](/typoqsuat)")
-         print(f"Added home button to {file}!\n")
+         print(f"{Fore.GREEN}Added{Style.RESET_ALL} home button to {file}!\n")
         else:
-         print(f"Skipped {file}.\n")
+         print(f"{Fore.YELLOW}Skipped{Style.RESET_ALL} {file}.\n")
    except:
       pass
 
 for i in glob.glob(f"{os.getcwd()}/*"):
  if i != f"{os.getcwd()}/index.md":
   addhome(i)
-print("Generating home buttons for entries.")
+print(f"{Fore.BLUE}>>>{Style.RESET_ALL} Generating home buttons for entries.")
 
 for i in glob.glob(f"{os.getcwd()}/typo/*"):
   addhome(i)
